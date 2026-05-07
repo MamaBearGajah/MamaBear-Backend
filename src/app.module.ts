d0a4a94ac54af016c79e0b00e05ffdb85e2b6b41
Anindya <anindyanitisarads@gmail.com>
2026-05-08 03:33:39 +0700

    feat: implement full crud for products, variants, and images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot([{ ttl: 60000, limit: 60 }])
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 60 }]),
+    ProductsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,64 @@
-export class CreateProductDto {}
+import { Type } from "class-transformer";
+import { IsArray, IsBoolean, IsEnum, IsNotEmpty, isNumber, IsNumber, IsOptional, IsString, Min, MinLength, ValidateNested } from "class-validator";
+import { CreateVariantDto } from "./create-variant.dto";
+import { CreateImageDto } from "./create-image.dto";
+
+enum ProductStatus {
+    active = 'active',
+    inactive = 'inactive',
+    draft = 'draft',
+}
+
+export class CreateProductDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    name!: string
+
+    @IsString()
+    @IsNotEmpty()
+    slug: string
+
+    @IsString()
+    @IsOptional()
+    description?: string
+    
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(0)
+    basePrice!: number
+
+    @IsNumber()
+    @IsOptional()
+    discountPrice?: number
+
+    @IsNumber()
+    @Min(1)
+    weight: number
+    
+    @IsString()
+    sku: string
+
+    @IsNumber()
+    stock: number
+
+    @IsString()
+    mainImage: string
+
+    @IsEnum(ProductStatus)
+    status: ProductStatus
+    
+    @IsString()
+    @IsOptional()
+    categoryId?: string
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => CreateImageDto)
+    images?: CreateImageDto[]
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => CreateVariantDto)
+    variants?: CreateVariantDto[]
+}
