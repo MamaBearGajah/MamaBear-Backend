b7d0c978dc9d99418a07e73e4a5aaf06aac7a274
egaherawati10 <ega.herawati81@gmail.com>
2026-06-18 14:52:51 +0700

    fix(banner): fixing timestamp error

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,17 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AdminConsultationsService } from './admin-consultations/admin-consultations.service';
-import { AdminConsultationsController } from './admin-consultations/admin-consultations.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+
+import { AdminConsultationsController } from './admin-consultations/admin-consultations.controller';
+import { AdminConsultationsService } from './admin-consultations/admin-consultations.service';
+
 import { AdminProductsController } from './admin-products/admin-products.controller';
 import { AdminProductsService } from './admin-products/admin-products.service';
+
 import { AdminCategoriesController } from './admin-categories/admin-categories.controller';
 import { AdminCategoriesService } from './admin-categories/admin-categories.service';
+
 import { AdminCustomersController } from './admin-customers/admin-customers.controller';
 import { AdminCustomersService } from './admin-customers/admin-customers.service';
 
+// import { AdminOrdersController } from './admin-orders/admin-orders.controller';
+// import { AdminOrdersService } from './admin-orders/admin-orders.service';
+
 @Module({
   imports: [PrismaModule],
-  controllers: [AdminConsultationsController, AdminProductsController, AdminCategoriesController, AdminCustomersController],
-  providers: [AdminConsultationsService, AdminProductsService, AdminCategoriesService, AdminCustomersService],
+  controllers: [
+    AdminConsultationsController,
+    AdminProductsController,
+    AdminCategoriesController,
+    AdminCustomersController,
+    // AdminOrdersController,
+  ],
+  providers: [
+    AdminConsultationsService,
+    AdminProductsService,
+    AdminCategoriesService,
+    AdminCustomersService,
+    // AdminOrdersService,
+  ],
 })
 export class AdminModule {}
